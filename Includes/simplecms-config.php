142a54d803e073aeb63e17f6ca3db99365b5a9dc
stef-cola <stefania.cola1@gmail.com>
2016-10-16 15:45:29 +1100

    Initial Starter Site Commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms9');
+    define('DB_USER', 'simplecmsuser9');
+    define('DB_PASSWORD', '.GrlXRaI!DZW');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'password');
+?>
